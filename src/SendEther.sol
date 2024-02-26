c5bcbdafd5992cc23e4d5a4c255c360307901d6a
fan ka <kekedog@126.com>
2024-02-26 21:17:32 +0800

    Update SendEther.sol

--- src/SendEther.sol
+++ src/SendEther.sol
@@ -7,6 +7,7 @@ contract SendEther {
     
     function transferEther(uint256 amount, address to) external {
         assembly {
+            // `(bool success, ) = to().call{value: amount}("")
             let s := call(gas(), to, amount, 0x00, 0x00, 0x00, 0x00)
             if iszero(s) {
                 revert(0x00, 0x00)
